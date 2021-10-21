f4c3b52daa1df7e7b206757c8d1deb597647bd64
Jiaxiao Zhou <duibao55328@gmail.com>
2021-10-21 08:14:12 +0000

    added filter

--- main.go
+++ main.go
@@ -33,16 +33,74 @@ var metadata_list = []metadata{
 				Email: "apptwo@hotmail.com",
 			},
 		},
-		Company:     "Random Inc.",
-		Website:     "https://website.com",
+		Company:     "Upbound Inc.",
+		Website:     "https://upbound.io",
 		Source:      "https://github.com/upbound/repo",
 		License:     "Apache-2.0",
+		Description: "### Why app 2 is the best\nBecause it simply is...",
+	},
+	{
+		Title:   "Valid App 1",
+		Version: "0.0.1",
+		Maintainers: []maintainer{
+			{
+				Name:  "firstmaintainer app1",
+				Email: "apptwo@hotmail.com",
+			},
+			{
+				Name:  "secondmaintainer app1",
+				Email: "secondmaintainer@gmail.com",
+			},
+		},
+		Company:     "Random Inc.",
+		Website:     "https://website.com",
+		Source:      "https://github.com/random/repo",
+		License:     "Apache-1.0",
 		Description: "### Why app 2 is the best\nBecause it simply is...",
 	},
 }
 
 func get_metadata(c *gin.Context) {
-	c.YAML(http.StatusOK, metadata_list)
+	title := c.Query("title")
+	version := c.Query("version")
+	company := c.Query("company")
+	website := c.Query("website")
+	source := c.Query("source")
+	license := c.Query("license")
+	maintainer_name := c.Query("maintainer_name")
+	maintainer_email := c.Query("maintainer_email")
+
+	var metadata_list_filtered []metadata
+	for _, metadata := range metadata_list {
+		if title != "" && metadata.Title != title {
+			continue
+		}
+		if version != "" && metadata.Version != version {
+			continue
+		}
+		if company != "" && metadata.Company != company {
+			continue
+		}
+		if website != "" && metadata.Website != website {
+			continue
+		}
+		if source != "" && metadata.Source != source {
+			continue
+		}
+		if license != "" && metadata.License != license {
+			continue
+		}
+		for _, maintainer := range metadata.Maintainers {
+			if maintainer_name != "" && maintainer.Name != maintainer_name {
+				continue
+			}
+			if maintainer_email != "" && maintainer.Email != maintainer_email {
+				continue
+			}
+		}
+		metadata_list_filtered = append(metadata_list_filtered, metadata)
+	}
+	c.YAML(http.StatusOK, metadata_list_filtered)
 }
 
 func post_metadata(c *gin.Context) {
